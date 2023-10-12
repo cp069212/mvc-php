f99dc83a7d67a1b5471e77895504892b7c93f37b
cp069212 <Clement_Papillon@etu.u-bourgogne.fr>
2023-10-12 08:18:34 +0200

    Isolez l'accès aux données

--- index.php
+++ index.php
@@ -1,51 +1,8 @@
 <?php
-      // Connexion à la base de données db.sql
-      try
-      {
-         $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-      }
-      catch(Exception $e){
-            die( 'Erreur : '.$e->getMessage()   );
-      }
 
-      // On récupère les 5 derniers billets
-      $statement = $database->query(
-         "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5");
-      
-      $posts = [];
-      while ($row = $statement->fetch()) {
-         $post = [
-            'title' => $row['titre'],
-            'content' => $row['contenu'],
-            'frenchCreationDate' => $row['date_creation_fr'],
-         ];
+require('src/model.php');
 
-         $posts[] = $post;
-      }
-?>
-
-<?php
-
-// We connect to the database.
-try {
-   $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','root', 'root');
-} catch(Exception $e) {
-   die('Erreur : '.$e->getMessage());
-}
-
-// We retrieve the 5 last blog posts.
-$statement = $database->query(
-   "SELECT id, titre, contenu, DATE_FORMAT(date_creation,'%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5");
-$posts = [];
-while (($row = $statement->fetch())) {
-   $post = [
-      'title' => $row['titre'],
-      'frenchCreationDate' => $row['date_creation_fr'],
-      'content' => $row['contenu'],
-   ];
-   
-   $posts[] = $post;
-}
+$posts = getPosts();
 
 require('templates/homepage.php');
 ?>
