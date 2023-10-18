e931051f800fe4f7981500769b5f48e2d534bc8d
Clement PAPILLON <cp069212@iut21.u-bourgogne.fr>
2023-10-18 15:20:00 +0200

    Gérez les erreurs

--- index.php
+++ index.php
@@ -5,30 +5,34 @@ require_once('blog/src/controllers/add_comment.php');
 require_once('blog/src/controllers/homepage.php');
 require_once('blog/src/controllers/post.php');
 
-if (isset($_GET['action']) && $_GET['action'] !== '') {
-    if ($_GET['action'] === 'post') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            $identifier = $_GET['id'];
-            
-            post($identifier);
+try{
+    if (isset($_GET['action']) && $_GET['action'] !== '') {
+        if ($_GET['action'] === 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+                
+                post($identifier);
+            } else {
+                echo 'Erreur : aucun identifiant de billet envoyé';
+                
+                die;
+            }
+        } elseif ($_GET['action'] === 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                $identifier = $_GET['id'];
+                
+                addComment($identifier, $_POST);
+            } else {
+                echo 'Erreur : aucun identifiant de billet envoyé';
+                
+                die;
+            }
         } else {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-            
-            die;
-        }
-    } elseif ($_GET['action'] === 'addComment') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            $identifier = $_GET['id'];
-            
-            addComment($identifier, $_POST);
-        } else {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-            
-            die;
+            echo "Erreur 404 : la page que vous recherchez n'existe pas.";
         }
     } else {
-        echo "Erreur 404 : la page que vous recherchez n'existe pas.";
+        homepage();
     }
-} else {
-    homepage();
+} catch (Exception $e) { // S'il y a eu une erreur, alors...
+    echo 'Erreur : '.$e->getMessage();
 }
